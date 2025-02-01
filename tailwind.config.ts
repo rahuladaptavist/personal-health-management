7b30248a674cd42055d490bf350609813a1cb725
Rahul Singh <rsingh@adaptavist.com>
2025-02-01 14:30:00 +0000

    fix: added flowbite login

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
+import { content, plugin } from "flowbite-react/tailwind";
+import colors from "tailwindcss/colors";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    content(),
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: colors.blue,
       },
     },
   },
-  plugins: [],
+  plugins: [plugin],
 } satisfies Config;
